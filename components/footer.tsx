76621e9f619f6d0b2078f8eb965f9835ea8354be
Arc Angelo Ibalio <gentlyawesome@gmail.com>
2021-12-19 00:24:08 +0800

    added pages

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+        Test Footer
+    </footer>
+  );
+}
+
+
--- pages/token-sale.tsx
+++ pages/token-sale.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import Footer from "../components/footer";
 import NavBar from "../components/navbar";
 
-const Home: NextPage = () => {
+const TokenSale: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Ark of Dreams</title>
+        <title>Ark of Dreams - Token Sale</title>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -16,12 +17,11 @@ const Home: NextPage = () => {
 
       <NavBar />
       <main>
-        <h1 className="text-3xl font-bold underline">Ark of Dreams Home Page</h1>
+        <h1 className="text-3xl font-bold underline">Token Page</h1>
       </main>
       <Footer />
-
     </div>
   );
 };
 
-export default Home;
+export default TokenSale;
