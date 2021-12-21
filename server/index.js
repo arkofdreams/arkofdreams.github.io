2cb9962963e67c6ebd8856006926d7789d4d77f0
Christian Blanquera <chris@incept.asia>
2021-12-21 21:49:54 +0800

    adding dev server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(express.static(path.resolve(__dirname, '../docs')));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
